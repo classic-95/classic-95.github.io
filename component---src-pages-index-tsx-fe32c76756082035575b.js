78744b5b5d95059916c89878c101cc9bf73d84cf
millo-L <seungmin3837@naver.com>
2023-11-24 19:55:17 +0900

    Updates

--- component---src-pages-index-tsx-fe32c76756082035575b.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkClassic95=self.webpackChunkClassic95||[]).push([[691],{7200:function(e,n,t){t.r(n),t.d(n,{Head:function(){return r},default:function(){return a}});var c=t(7294),s=t(6405),u=t(9878);function a(){return c.createElement(s.Z,{lang:"ko"})}const r=()=>c.createElement(u.Z,{title:"Classic95의 블로그"})}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-fe32c76756082035575b.js.map
